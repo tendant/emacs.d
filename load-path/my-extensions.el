029f90aef3ab99bd03a1c86af32e4e18d40610be
Lei <tendant@gmail.com>
2015-07-14 17:53:07 -0700

    add web-mode

--- load-path/my-extensions.el
+++ load-path/my-extensions.el
@@ -239,7 +239,8 @@
 (setq js-indent-level 4)
 ;; (add-to-list 'auto-mode-alist '("\\.js.\\'" . js-mode))
 (autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js.\\'" . js2-mode)) ; \' will match empty string only at the end of the string or buffer; \$ will match end of line
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)) ; \' will match empty string only at the end of the string or buffer; \$ will match end of line
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 ;; (setq js2-consistent-level-indent-inner-bracket-p t)
 ;; (setq js2-pretty-multiline-decl-indentation-p t)
 ;; (put 'narrow-to-region 'disabled nil)
