56722c9c66a8e79d0e4c153f37619bfb5f8749bb
Lei <tendant@gmail.com>
2015-07-15 15:28:10 -0700

    Do not revert buffer in magit

--- load-path/my-extensions.el
+++ load-path/my-extensions.el
@@ -118,6 +118,7 @@
 (require 'magit)
 (setq magit-last-seen-setup-instructions "1.4.0")
 (setq magit-auto-revert-mode nil)
+(setq magit-revert-buffers nil)
 (require 'magit-svn)
 
 ;; git minor mode
