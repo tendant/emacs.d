f877c73dd6feb89d4e18f6057b7b5197b7c4fc64
Lei Wang <tendant@gmail.com>
2020-10-14 10:07:56 -0700

    update path

--- load-path/my-reveal.el
+++ load-path/my-reveal.el
@@ -1,4 +1,4 @@
 (require 'ox-reveal)
-(setq org-reveal-root "file:///Users/l/emacs.d/load-path/ox-reveal/js/reveal.js")
+(setq org-reveal-root "file://~/.emacs.d/load-path/ox-reveal/reveal.js")
 
 (provide 'my-reveal)
