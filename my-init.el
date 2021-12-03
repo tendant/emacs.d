ebedaeefa9cb4879054fdfc457217d1b0bb8420e
Lei Wang <tendant@gmail.com>
2021-12-02 17:34:41 -0800

    migrated basic config

--- my-init.el
+++ my-init.el
@@ -16,119 +16,6 @@
 ;; (add-to-list 'package-archives
 ;;              '("melpa" . "https://melpa.org/packages/"))
 
-(setq package-archives
-      '(("org" . "http://orgmode.org/elpa/")
-        ("gnu" . "http://elpa.gnu.org/packages/")
-	("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("melpa" . "http://melpa.org/packages/")
-	;; ("melpa-stable" . "http://stable.melpa.org/packages/")
-	))
-
-(package-initialize)
-
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  ;; ((and )dd-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-  (add-to-list 'package-archives
-               '("melpa-stable" . "https://stable.melpa.org/packages/") t))
-;; (package-initialize) ;; You might already have this line
-  
-
-(defvar my-packages '(ac-cider
-                      org-chef
-                      cider
-                      clojure-mode
-                      clojure-snippets
-                      cnfonts
-                      company
-                      company-ansible
-                      company-web
-                      ;; company-lsp
-                      counsel
-                      dart-mode
-                      default-text-scale
-                      dockerfile-mode
-                      dracula-theme
-                      edwina
-                      ein
-                      ejc-sql
-                      ;; el-get
-                      exec-path-from-shell
-                      flycheck
-                      flycheck-clojure
-                      flycheck-rust
-                      go-mode
-                      graphql-mode
-                      graphviz-dot-mode
-                      helm
-                      helm-ls-git
-                      ivy
-                      js2-mode
-                      rjsx-mode
-                      ledger-mode
-                      lsp-mode
-                      lsp-ui
-                      magit
-                      magit-svn
-                      markdown-mode
-                      mu4e-alert
-                      notmuch
-                      nord-theme
-                      org
-                      org-gcal
-                      ;; org-plus-contrib
-                      org-roam
-                      ox-hugo
-                      ox-reveal
-                      plantuml-mode
-                      pyim
-                      pyim-basedict
-                      restclient
-                      rego-mode
-                      rg
-                      rust-mode
-                      ;; rust-cargo
-                      flycheck-rust
-                      skeletor
-                      sis
-                      smartparens
-                      smtpmail-multi
-                      solarized-theme
-                      solidity-mode
-                      swift-mode
-                      use-package
-                      vterm
-                      w3m
-                      web-mode
-                      ws-butler
-                      yaml-mode
-                      yasnippet
-                      yasnippet-snippets))
-
-(defvar my-emacs26-packages '(posframe))
-
-; fetch the list of packages available 
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(setq n 0)                                  ; set n as 0
-(dolist (pkg my-packages)               ; for each pkg in list
-  (unless (or                               ; unless
-           (package-installed-p pkg)        ; pkg is installed or
-           (assoc pkg                       ; pkg is in the archive list
-                  package-archive-contents))
-    (setq n (+ n 1))))                      ; add one to n
-(when (> n 0)                               ; if n > 0,
-  (package-refresh-contents))               ; refresh packages
-
-(dolist (p my-packages)
-  (unless (package-installed-p p)
-    (package-install p)))
-
-(if (not (version< emacs-version "26"))
-    (dolist (p my-emacs26-packages)
-      (unless (package-installed-p p)
-	(package-install p))))
 
 
 ;;; Add el-get
@@ -153,12 +40,6 @@
 ;; (el-get 'sync)
 
 
-(if (and
-     (< (string-to-number (car (split-string (org-version) "\\."))) 8)
-     (y-or-n-p "Package org-mode does not meet requirement. Install it? "))
-    (package-install 'org))
-
-
 (let ((current-dir (file-name-directory load-file-name)))
 
   (defun load-config(conf)
@@ -169,7 +50,104 @@
   (load-config "load-path/config-basic.org")
   (load-config "load-path/config-extension.org")
 
-  (load (concat current-dir "load-path/my-basic-config.el"))
-  (message "Loaded my-basic-config.el")
-  (load (concat current-dir "load-path/my-extensions.el"))
-  (message "Loaded my-extensions.el"))
+  ;;(load (concat current-dir "load-path/my-basic-config.el"))
+  ;;(message "Loaded my-basic-config.el")
+  ;; (load (concat current-dir "load-path/my-extensions.el"))
+  ;; (message "Loaded my-extensions.el")
+  )
+
+;; (defvar my-packages '(ac-cider
+;;                       org-chef
+;;                       cider
+;;                       clojure-mode
+;;                       clojure-snippets
+;;                       cnfonts
+;;                       company
+;;                       company-ansible
+;;                       company-web
+;;                       ;; company-lsp
+;;                       counsel
+;;                       dart-mode
+;;                       default-text-scale
+;;                       dockerfile-mode
+;;                       dracula-theme
+;;                       edwina
+;;                       ein
+;;                       ejc-sql
+;;                       ;; el-get
+;;                       exec-path-from-shell
+;;                       flycheck
+;;                       flycheck-clojure
+;;                       flycheck-rust
+;;                       go-mode
+;;                       graphql-mode
+;;                       graphviz-dot-mode
+;;                       helm
+;;                       helm-ls-git
+;;                       ivy
+;;                       js2-mode
+;;                       rjsx-mode
+;;                       ledger-mode
+;;                       lsp-mode
+;;                       lsp-ui
+;;                       magit
+;;                       magit-svn
+;;                       markdown-mode
+;;                       mu4e-alert
+;;                       notmuch
+;;                       nord-theme
+;;                       org
+;;                       org-gcal
+;;                       ;; org-plus-contrib
+;;                       org-roam
+;;                       ox-hugo
+;;                       ox-reveal
+;;                       plantuml-mode
+;;                       pyim
+;;                       pyim-basedict
+;;                       restclient
+;;                       rego-mode
+;;                       rg
+;;                       rust-mode
+;;                       ;; rust-cargo
+;;                       flycheck-rust
+;;                       skeletor
+;;                       sis
+;;                       smartparens
+;;                       smtpmail-multi
+;;                       solarized-theme
+;;                       solidity-mode
+;;                       swift-mode
+;;                       use-package
+;;                       vterm
+;;                       w3m
+;;                       web-mode
+;;                       ws-butler
+;;                       yaml-mode
+;;                       yasnippet
+;;                       yasnippet-snippets))
+
+;; (defvar my-emacs26-packages '(posframe))
+
+;; ; fetch the list of packages available
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
+
+;; (setq n 0)                                  ; set n as 0
+;; (dolist (pkg my-packages)               ; for each pkg in list
+;;   (unless (or                               ; unless
+;;            (package-installed-p pkg)        ; pkg is installed or
+;;            (assoc pkg                       ; pkg is in the archive list
+;;                   package-archive-contents))
+;;     (setq n (+ n 1))))                      ; add one to n
+;; (when (> n 0)                               ; if n > 0,
+;;   (package-refresh-contents))               ; refresh packages
+
+;; (dolist (p my-packages)
+;;   (unless (package-installed-p p)
+;;     (package-install p)))
+
+;; (if (not (version< emacs-version "26"))
+;;     (dolist (p my-emacs26-packages)
+;;       (unless (package-installed-p p)
+;; 	(package-install p))))
